f36b8f16fbc3d170c49c9ca979bceaab8aac33f4
Leandro Simões <lesimoes0@gmail.com>
2021-06-01 18:23:36 -0300

    vai

--- controllers/usuarios.controller.php
+++ controllers/usuarios.controller.php
@@ -33,5 +33,32 @@ class UsuariosController extends Controller {
         }
     }
 
+    public function edit () {
+        $params = App::getRouter()->getParams();
+
+        if ($_POST) {
+            $id = $_POST['id'];
+            if (isset($id)) {
+                $data = $_POST;
+                $this->model->updateById($id, $data);
+            }
+        }
+
+        if ( isset($params[0]) ) {
+            $id = strtolower($params[0]);
+            $this->data['usuario'] = $this->model->getById($id);
+            $this->data['cartoes'] = $this->cartoesModel->getByUsuarioId($id);
+        }
+    }
+
+    public function add () {
+        $params = App::getRouter()->getParams();
+
+        if ($_POST) {
+            $data = $_POST;
+            $this->model->insert($data);
+        }
+    }
+
 
 }
--- models/usuario.php
+++ models/usuario.php
@@ -15,4 +15,29 @@ class Usuario extends Model {
         $result = $this->db->query($sql);
         return isset($result[0]) ? $result[0] : null;
     }
+
+    public function updateById($id, $data) {
+        $id = $this->db->escape($id);
+
+        $nome = $this->db->escape($data['nome']);
+        $tipo = $this->db->escape($data['tipo']);
+        $nascimento = $this->db->escape($data['nascimento']);
+        $documento = $this->db->escape($data['documento']);
+
+        $sql = "UPDATE usuario SET nome = '$nome', data_nascimento = '$nascimento', tipo = '$tipo', cpf_cnpj = '$documento' WHERE id = '$id';";
+        $result = $this->db->query($sql);
+        return $result;
+    }
+
+    public function insert($data) {
+        $nome = $this->db->escape($data['nome']);
+        $tipo = $this->db->escape($data['tipo']);
+        $nascimento = $this->db->escape($data['nascimento']);
+        $documento = $this->db->escape($data['documento']);
+
+        $sql = "INSERT INTO usuario (nome, tipo, data_nascimento, cpf_cnpj) VALUES ('$nome', '$tipo', '$nascimento', '$documento');";
+        var_dump($sql);
+        $result = $this->db->query($sql);
+        return $result;
+    }
 }
